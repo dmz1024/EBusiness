d1fa2fd21df5e7e69c3a6877caa7d0f39c11d077
dmz1024 <894350911@qq.com>
2017-10-13 20:04:28 +0800

    微信支付

--- app/src/main/java/com/ediancha/edcbusiness/helper/pay/WeChatPayUtil.java
+++ app/src/main/java/com/ediancha/edcbusiness/helper/pay/WeChatPayUtil.java
@@ -6,6 +6,9 @@ import com.tencent.mm.opensdk.modelpay.PayReq;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
 /**
  * Created by dengmingzhi on 2017/10/11.
  */
@@ -14,18 +17,79 @@ public class WeChatPayUtil extends Pay {
     @Override
     public void start(Activity activity, Object object) {
         super.start(activity, object);
-        Data data= (Data) object;
+
+        EventBus.getDefault().register(this);
+
+        Data data = (Data) object;
         IWXAPI api = WXAPIFactory.createWXAPI(activity, null);
         api.registerApp("wxd930ea5d5a258f4f");
         PayReq request = new PayReq();
         request.appId = data.getAppid();
         request.partnerId = data.getPartnerid();
-        request.prepayId= data.getPrepayid();
+        request.prepayId = data.getPrepayid();
         request.packageValue = "Sign=WXPay";
-        request.nonceStr= data.getNoncestr();
-        request.timeStamp= data.getTimestamp();
-        request.sign= data.getSign();
+        request.nonceStr = data.getNoncestr();
+        request.timeStamp = data.getTimestamp();
+        request.sign = data.getSign();
         api.sendReq(request);
     }
 
+
+    public static class Data {
+        public String appid;
+        public String noncestr;
+        public String partnerid;
+        public String prepayid;
+        public String sign;
+        public String timestamp;
+
+        public String getAppid() {
+            return appid;
+        }
+
+        public String getNoncestr() {
+            return noncestr;
+        }
+
+        public String getPartnerid() {
+            return partnerid;
+        }
+
+        public String getPrepayid() {
+            return prepayid;
+        }
+
+        public String getSign() {
+            return sign;
+        }
+
+        public String getTimestamp() {
+            return timestamp;
+        }
+    }
+
+    public static class WeChatPayEvent {
+        private int type;
+
+        public WeChatPayEvent(int type) {
+            this.type = type;
+        }
+    }
+
+
+    @Subscribe
+    public void onPayResult(WeChatPayEvent weChatPayEvent) {
+        EventBus.getDefault().unregister(this);
+        switch (weChatPayEvent.type) {
+            case 1:
+                iPayResultInterface.onSuccess();
+                break;
+            case 2:
+                iPayResultInterface.onCancel();
+                break;
+            case 3:
+                iPayResultInterface.onFaile("支付失败,可联系客服咨询");
+                break;
+        }
+    }
 }
--- app/src/main/java/com/ediancha/edcbusiness/wxapi/WXPayEntryActivity.java
+++ app/src/main/java/com/ediancha/edcbusiness/wxapi/WXPayEntryActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.dmz.library.dmzapi.utils.MyToast;
+import com.ediancha.edcbusiness.helper.pay.WeChatPayUtil;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
+
+import org.greenrobot.eventbus.EventBus;
 
 
 public class WXPayEntryActivity extends Activity {
@@ -24,19 +27,17 @@ public class WXPayEntryActivity extends Activity {
 
     private void handleIntent(Intent intent) {
         SendAuth.Resp resp = new SendAuth.Resp(intent.getExtras());
+        int result = 1;
         if (resp.errCode == 0) {
-//            MyToast.showToast("支付成功");
-
+            result = 1;
         } else if (resp.errCode == -2) {
-//            MyToast.showToast("用户取消");
-
+            result = 2;
         } else {
-//            MyToast.showToast("支付失败");
-
+            result = 3;
         }
+        EventBus.getDefault().post(new WeChatPayUtil.WeChatPayEvent(result));
         finish();
     }
-
 
 
 }
